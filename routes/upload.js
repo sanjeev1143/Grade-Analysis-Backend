1300cc87413475d1283104443d446caa99a56117
unknown <sanjeevreddy@trikon.io>
2023-07-07 23:43:10 +0530

    first commit

---
+++ routes/upload.js
@@ -0,0 +1,178 @@
+// Import dependencies
+import multer from 'multer';
+import path from 'path';
+import Express from 'express';
+import fs from "fs";
+import PDFParser from "pdf2json";
+import { log } from 'console';
+const router = Express.Router();
+
+
+
+
+
+const pdfParser = new PDFParser();
+// Get all the filenames from the patients folder
+const files = fs.readdirSync("../images");
+
+// All of the parse patients
+let patients = [];
+
+// Make a IIFE so we can run asynchronous code
+
+
+
+async function getPdf() {
+
+    // Await all of the patients to be passed
+    // For each file in the patients folder
+    await Promise.all(files.map(async (file) => {
+
+        // Set up the pdf parser
+        let pdfParser = new PDFParser(this, 1);
+
+        // Load the pdf document
+        pdfParser.loadPDF(`./images/${file}`);
+
+        // Parsed the patient
+        let patient = await new Promise(async (resolve, reject) => {
+
+            // On data ready
+            pdfParser.on("pdfParser_dataReady", (pdfData) => {
+
+                // The raw PDF data in text form
+                const raw = pdfParser.getRawTextContent().replace(/\r\n/g, " ");
+                // console.log(raw.substring(530,770));
+                
+                //771
+                
+                
+                
+                // NameSubjectTypeCreditGrade
+                const start = raw.indexOf("NameSubjectTypeCreditGrade") + "NameSubjectTypeCreditGrade".length;
+                const end = raw.indexOf("The result is provisional")
+                const markslist =  (raw.substring(start,1353).split(" "));
+               //console.log(raw);
+                // console.log(markslist[0].charCodeAt(0)); 819 1353
+
+                const subcode = []
+                const grade =[]
+                const sub = []
+                let prv = "";
+                let count = 0;
+
+                for(let i of markslist){
+                    if(i!==""){
+                        let range = i[0].charCodeAt(0)-48;
+                        
+                        if(range>=0 && range<=9 ){
+                            count++;
+                            let len = (""+count).length;
+                            subcode.push(i.substring(0+len,10+len));
+                            prv+=i.substring(10+len)
+                        }
+                        else{
+                            let ter = i.charCodeAt(i.length-2) - 48
+                            // console.log(prv);
+                            if(ter>=0 && ter<=9){
+                                let subject = (prv+i);
+                                sub.push(subject.substring(0,subject.length-2));
+                                grade.push(subject.substring(subject.length-2))
+                                prv = "";
+                            }
+                            else
+                                prv+=i;
+                        }
+
+                    }
+        
+                }
+
+
+
+                const final = []
+                for(let i =0;i<sub.length;i++)
+                    final.push({
+                        subject_code:subcode[i],
+                        subject_name:sub[i],
+                        grade:grade[i]
+                    })
+                // console.log(final);
+                resolve({
+                    final
+                })
+
+
+
+            });
+
+        });
+
+        // Add the patient to the patients array
+        patients.push(patient);
+
+    }));
+
+    // Save the extracted information to a json file
+    // fs.writeFileSync("patients.json", JSON.stringify(patients));
+    return JSON.stringify(patients);
+
+}; 
+
+
+
+
+
+
+
+
+const storageEngine = multer.diskStorage({
+    destination: "./images",
+    filename: (req, file, cb) => {
+      cb(null, `resume.pdf`);
+    },
+  });
+  
+  const upload = multer({
+    storage: storageEngine,
+  });
+  
+  const checkFileType = function (file, cb) {
+    //Allowed file extensions
+    const fileTypes = /jpeg|jpg|png|gif|svg/;
+  
+    //check extension names
+    const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  
+    const mimeType = fileTypes.test(file.mimetype);
+  
+    if (mimeType && extName) {
+      return cb(null, true);
+    } else {
+      cb("Error: You can Only Upload Images!!");
+    }
+  };
+  
+
+
+  router.post("/single", upload.single("image"), async (req, res) => {
+    if (req.file) {
+  console.log(req.file);
+
+   const result =  await getPdf();
+   console.log(result);
+
+//    if(result && fs.existsSync('./images/resume.pdf'))  fs.unlinkSync('./images/resume.pdf')
+res.send(result)
+
+ 
+    //   res.send("Single file uploaded successfully");
+      
+    } else {
+      res.status(400).send("Please upload a valid image");
+    }
+  });
+
+
+  export default  router;
+  
---
+++ server.js
@@ -0,0 +1,51 @@
+import Express from 'express';
+import Mongoose from 'mongoose';
+import  CorsOptions  from 'cors';
+import  BodyParser  from 'body-parser';
+import multer from 'multer';
+import path from 'path';
+import  uploadroute from './routes/upload.js'
+
+
+
+const app = Express();
+const db = "mongodb+srv://Sanjeev:Sanju266@cluster0.erlocnt.mongodb.net/FullStack?retryWrites=true&w=majority";
+// const multer = require('multer');
+// const path = require('path');
+
+app.use(CorsOptions());
+app.use(Express.json());
+
+// parse application/x-www-form-urlencoded
+app.use(BodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(BodyParser.json());
+
+
+
+
+
+
+Mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
+});
+
+// console.log(uploadroute);
+app.use('/upload',uploadroute);
+
+
+app.get('/', (req, res) => {
+  res.json({
+    name: 'Rest API Backend Server Running Live'
+  });
+});
+
+app.listen(8000, () => {
+  console.log('Listening on port 8000');
+});
